c600ed0cf255846daee4cb2ff6eeaf46529456fe
SwapnilKumbhar <swapsk05@gmail.com>
2018-04-14 11:14:29 +0530

    Added decoy logic

--- honeywords.py
+++ honeywords.py
@@ -2,6 +2,7 @@
 The purpose of this class is to perform all things related to decoy passwords.
 '''
 import json
+import random
 
 from DBDriver import *
 from auxClient import *
@@ -18,8 +19,40 @@ class Honeywords:
         self.aux_client = AuxClient(aux_config['host'], aux_config['pass'])
         return
     
-    def addUser(self):
-        # Add user with decoy passwords
+    # Retrieves a random password from the given dictionary
+    def _generateDecoy(self, config):
+        # Load Configurations and the Dictionary
+        pass_list = open(config['dictionary'], 'r')
+        dict_len = int(config['dict_length'])
+
+        # Seek to random location in dictionary file and give that password
+        seek = random.randint(1,dict_len)
+        # Get random offset from chosen location
+        offset = random.randint(1,20)
+        pass_list.seek(seek)
+        pass_list.next()
+        for i in range(0,offset - 1):
+            pass_list.next()
+        decoy = pass_list.next()
+
+        # Remove the linefeed at the end and return
+        return decoy.replace('\n','')
+
+    # Add user with decoy passwords
+    def addUser(self, password):
+        # Generate fake passwords list
+        pass_list = []
+        honey_config = self.config['honeywords']
+        for i in range(0, int(honey_config['decoy_count']) + 1):
+            pass_list.append(self._generateDecoy(honey_config))
+
+        # At random position in the list, add the real password
+        position = random.randint(0,int(honey_config['decoy_count']) + 1)
+        pass_list[position] = password
+
+        # Perform database operations
+        
+
         return
 
     def validateUser(self):
